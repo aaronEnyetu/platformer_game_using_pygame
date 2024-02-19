1509552b5bdb9d4e460b439ab5ae1fc75d589d32
Aaron Enyetu <aaronenyetu@gmail.com>
2024-02-18 16:35:51 -0800

    Added enemies

--- platformerGame.py
+++ platformerGame.py
@@ -37,6 +37,8 @@ class Player():
 		self.rect = self.image.get_rect()
 		self.rect.x = x
 		self.rect.y = y
+		self.width = self.image.get_width()
+		self.height = self.image.get_height()
 		self.vel_y = 0
 		self.jumped = False
 		self.direction = 0
@@ -89,6 +91,23 @@ class Player():
 		dy += self.vel_y
 
 		#check for collision
+		for tile in world.tile_list:
+			#check for collision in x direction
+			if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
+				dx = 0
+			#check for collision in y direction
+			if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
+				#check if below the ground i.e. jumping
+				if self.vel_y < 0:
+					dy = tile[1].bottom - self.rect.top
+					self.vel_y = 0
+				#check if above the ground i.e. falling
+				elif self.vel_y >= 0:
+					dy = tile[1].top - self.rect.bottom
+					self.vel_y = 0
+
+
+
 
 		#update player coordinates
 		self.rect.x += dx
@@ -100,6 +119,7 @@ class Player():
 
 		#draw player onto screen
 		screen.blit(self.image, self.rect)
+		pygame.draw.rect(screen, (255, 255, 255), self.rect, 2)
 
 
 
@@ -130,12 +150,38 @@ class World():
 					img_rect.y = row_count * tile_size
 					tile = (img, img_rect)
 					self.tile_list.append(tile)
+				if tile == 3:
+					blob = Enemy(col_count * tile_size, row_count * tile_size + 15)
+					blob_group.add(blob)
 				col_count += 1
 			row_count += 1
 
 	def draw(self):
 		for tile in self.tile_list:
 			screen.blit(tile[0], tile[1])
+			pygame.draw.rect(screen, (255, 255, 255), tile[1], 2)
+
+
+
+class Enemy(pygame.sprite.Sprite):
+	def __init__(self, x, y):
+		pygame.sprite.Sprite.__init__(self)
+		self.image = pygame.image.load('img/blob.png')
+		self.rect = self.image.get_rect()
+		self.rect.x = x
+		self.rect.y = y
+		self.move_direction = 1
+		self.move_counter = 0
+
+	def update(self):
+		self.rect.x += self.move_direction
+		self.move_counter += 1
+		if abs(self.move_counter) > 50:
+			self.move_direction *= -1
+			self.move_counter *= -1
+
+
+
 
 
 
@@ -165,6 +211,9 @@ world_data = [
 
 
 player = Player(100, screen_height - 130)
+
+blob_group = pygame.sprite.Group()
+
 world = World(world_data)
 
 run = True
@@ -176,6 +225,9 @@ while run:
 	screen.blit(sun_img, (100, 100))
 
 	world.draw()
+
+	blob_group.update()
+	blob_group.draw(screen)
 
 	player.update()
 
